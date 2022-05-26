c7e162cf700dc836b9ee9deb170b85fada732e62
yukimark <mark2peace@gmail.com>
2022-05-26 10:47:10 +0000

    spend_index,create作成

--- app/controllers/spends_controller.rb
+++ app/controllers/spends_controller.rb
@@ -1,2 +1,25 @@
 class SpendsController < ApplicationController
+
+  def index 
+    @spends = Spend.all.order(created_at: :desc)
+    @spend = Spend.new
+  end
+
+  def create 
+    @spend = Spend.new(spend_params)
+    begin
+      @spend.save!
+      redirect_to action: 'index'
+      flash[:success] = '保存しました。'
+    rescue
+      flash.now[:danger] = @spend.error_message
+      @spends = Spend.all.order(created_at: :desc)
+      render :index
+    end
+  end
+
+  private
+    def spend_params
+      params.require(:spend).permit(:b_item, :c_item, :content, :price, :memo, :user_id)
+    end
 end
--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,8 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  # 何かを登録するときのエラーメッセージ
+  def error_message
+    self.errors.full_messages.join(',')
+  end
 end

5ddd4eeb005199de7a13f7c6ed7148edc2df56f0
yukimark <mark2peace@gmail.com>
2022-05-26 12:02:07 +0000

    annotate初期設定

--- app/models/spend.rb
+++ app/models/spend.rb
@@ -1,3 +1,18 @@
+# == Schema Information
+#
+# Table name: spends
+#
+#  id                     :bigint           not null, primary key
+#  b_item(大項目)         :string
+#  c_item(中項目)         :string
+#  content(内容)          :string
+#  deleted_at(論理削除用) :datetime
+#  memo(メモ)             :text
+#  price(金額)            :integer          not null
+#  created_at             :datetime         not null
+#  updated_at             :datetime         not null
+#  user_id(ユーザーID)    :integer          not null
+#
 class Spend < ApplicationRecord
   validates :price, presence: true
   validates :user_id, presence: true

dec61b5561074d453f73181e0ee84d6e9073a3e5
yukimark <mark2peace@gmail.com>
2022-05-26 11:52:50 +0000

    spend_編集、削除機能追加、yml対応

--- app/controllers/spends_controller.rb
+++ app/controllers/spends_controller.rb
@@ -9,13 +9,36 @@ class SpendsController < ApplicationController
     @spend = Spend.new(spend_params)
     begin
       @spend.save!
-      redirect_to action: 'index'
       flash[:success] = '保存しました。'
+      redirect_to action: 'index'
     rescue
       flash.now[:danger] = @spend.error_message
       @spends = Spend.all.order(created_at: :desc)
       render :index
     end
+  end
+
+  def edit
+    @spend = Spend.find(params[:id])
+  end
+
+  def update
+    @spend = Spend.find(params[:id])
+    begin
+      @spend.update!(spend_params)
+      flash[:success] = '保存しました。'
+      redirect_to spends_path
+    rescue 
+      flash.now[:danger] = @spend.error_message
+      render :edit
+    end
+  end
+
+  def destroy
+    @spend = Spend.find(params[:id])
+    @spend.destroy
+    flash[:success] = '削除しました。'
+    redirect_to spends_path
   end
 
   private

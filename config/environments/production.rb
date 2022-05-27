4af46505eea6bfc744227e48d2c4a062a89f562d
yukimark <mark2peace@gmail.com>
2022-05-27 07:02:44 +0000

    herokuの設定追加

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -117,4 +117,7 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+  # heroku用の設定
+  config.action_mailer.default_url_options = { :host => 'polar-meadow-7142.herokuapp.com' }
+
 end

adcb66bf48b8365aa6f3c117bb10f6cee8480f89
yukimark <mark2peace@gmail.com>
2022-06-04 11:10:06 +0000

    primary_item_listsテーブルに初期値判別用のカラム追加

--- db/migrate/20220603110506_create_primary_item_lists.rb
+++ db/migrate/20220603110506_create_primary_item_lists.rb
@@ -3,6 +3,7 @@ class CreatePrimaryItemLists < ActiveRecord::Migration[6.1]
     create_table :primary_item_lists, comment: '出費の大項目リスト' do |t|
       t.string :primary_item, null: false, comment: '大項目'
       t.integer :user_id, null: false, comment: 'current_user_idが入る'
+      t.boolean :initial_value, null: false, default: false, comment: '初期値ならtrue'
 
       t.timestamps
     end

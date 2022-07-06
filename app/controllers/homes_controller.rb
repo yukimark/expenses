09e2e77cfa64aca3cd7f84682c7ba10419ce6567
yukimark <mark2peace@gmail.com>
2022-07-06 05:33:06 +0000

    支出目標と比較が表示されるようにした

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -7,6 +7,7 @@ class HomesController < ApplicationController
     @spendsprevmonth = current_spends.where(created_at: d.prev_month.in_time_zone.all_month)
     @spendsum = @spendsthismonth.sum(:price)
     @primaryitemlists = current_primary_item_lists.order(:id)
+    @spend_targetvaluesum = @primaryitemlists.sum(:spend_target_value)
   end
 
   def setting; end
